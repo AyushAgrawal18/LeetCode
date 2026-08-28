9899895c0e864c1660698ace8c007f9c6e8ed38b
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-08-29 00:20:16 +0530

    Time: 23 ms (82.43%) | Memory: 26.6 MB (82.39%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
 
-    int solve(int i, int j, string &a, string &b, vector<vector<int>> &dp){
+    // int solve(int i, int j, string &a, string &b, vector<vector<int>> &dp){
 
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+    //     if(i<0 || j<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        int take=0;
-        if(a[i]==b[j]){
-            take = 1 + solve(i-1, j-1, a, b, dp);
-        }
-        int not_take = max(solve(i-1, j, a, b, dp), solve(i, j-1, a, b, dp));
+    //     int take=0;
+    //     if(a[i]==b[j]){
+    //         take = 1 + solve(i-1, j-1, a, b, dp);
+    //     }
+    //     int not_take = max(solve(i-1, j, a, b, dp), solve(i, j-1, a, b, dp));
 
 
-        return dp[i][j]=max(take, not_take);
-    }
+    //     return dp[i][j]=max(take, not_take);
+    // }
 
 
     int longestCommonSubsequence(string text1, string text2) {
